fc84ee14e6c4d8b9ae3e4a270d5f8a8ab5f921b1
Bisheshxx <bishesh.tuladhar1@gmail.com>
2022-04-25 18:26:11 +0545

    initiated connection to the api

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,27 @@
+import { GraphQLClient } from 'graphql-request';
 import React from 'react'
 import '../components/Table.css'
+import { UsersQuery, useUsersQuery } from '../generated';
 function Table() {
+    const graphqlClient = new GraphQLClient(
+        "https://graphqlzero.almansi.me/api"
+      );
+      const { data, isLoading, error } = useUsersQuery<UsersQuery, Error>(
+        graphqlClient,
+        {}
+      );
+    
+      if (isLoading) {
+        return <div className="box">Loading...</div>;
+      }
+    
+      if (error) {
+        return <div>Error!</div>;
+      }
+    
+      
   return (
+      <>
     <div className="row">
         <input type="checkbox" />
         <span>Name</span>
@@ -11,6 +31,16 @@ function Table() {
         <span>Website</span>
         <span>Address</span>
     </div>
+    {data?.users?.data?.map((u:any) => {
+        return (
+          <div className="bg-white rounded-lg shadow-md w-80 text-center">
+            <div className="mt-4 p-10 text-bold">
+              <p>user: {u.username}</p>
+            </div>
+          </div>
+        );
+      })}
+    </>
   )
 }
 
